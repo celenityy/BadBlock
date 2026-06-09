8bf399511555eaf182e4e19e784747d6032731cc
celenity <celenity@celenity.dev>
2026-06-09 03:46:46 +0000

    fix: update curl flags
    Notably:
    
    - Splits ciphers into separate categories for ones that should be applied to TLS 1.3 and ones that should be applied to non-TLS 1.3 connections (because curl requires/uses two separate options for this, so this ensures it handles/sets them properly)
    - Removes options that do not apply to HTTP(S) requests
    - Enables parallel downloads
    - Prevents curl from automatically converting POST requests to GET requests upon certain redirects
    - Sets curl to retry upon all errors (will fail after 5 tries)
    
    Signed-off-by: celenity <celenity@celenity.dev>

--- scripts/env_common.sh
+++ scripts/env_common.sh
@@ -201,12 +201,29 @@ readonly BADBLOCK_UV_TOOLS
 export BADBLOCK_UV_TOOLS
 
 # Cipher suites
-## (This enforces strong cipher suites - see ex. https://browserleaks.com/tls)
-readonly BADBLOCK_CIPHERS_DEFAULT='TLS_AES_128_GCM_SHA256:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_256_GCM_SHA384:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384'
-if [[ -z "${BADBLOCK_CIPHERS+x}" ]]; then
-    BADBLOCK_CIPHERS="${BADBLOCK_CIPHERS_DEFAULT}"
+## (These enforce strong cipher suites - see ex. https://browserleaks.com/tls)
+
+## For TLS 1.3 connections
+### https://curl.se/docs/manpage.html#--tls13-ciphers
+readonly BADBLOCK_TLS13_CIPHERS_DEFAULT='TLS_AES_128_GCM_SHA256:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_256_GCM_SHA384'
+if [[ -z "${BADBLOCK_TLS13_CIPHERS+x}" ]]; then
+  BADBLOCK_TLS13_CIPHERS="${BADBLOCK_TLS13_CIPHERS_DEFAULT}"
 fi
-readonly BADBLOCK_CIPHERS
+readonly BADBLOCK_TLS13_CIPHERS
+export BADBLOCK_TLS13_CIPHERS
+
+## For non-TLS 1.3 connections
+### https://curl.se/docs/manpage.html#--ciphers
+readonly BADBLOCK_NONTLS13_CIPHERS_DEFAULT='ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384'
+if [[ -z "${BADBLOCK_NONTLS13_CIPHERS+x}" ]]; then
+  BADBLOCK_NONTLS13_CIPHERS="${BADBLOCK_NONTLS13_CIPHERS_DEFAULT}"
+fi
+readonly BADBLOCK_NONTLS13_CIPHERS
+export BADBLOCK_NONTLS13_CIPHERS
+
+# This includes all ciphers (combining BADBLOCK_TLS13_CIPHERS + BADBLOCK_NONTLS13_CIPHERS)
+## Useful because many programs do not require specifying a separate set of ciphers for TLS 1.3 like curl does
+readonly BADBLOCK_CIPHERS="${BADBLOCK_TLS13_CIPHERS}:${BADBLOCK_NONTLS13_CIPHERS}"
 export BADBLOCK_CIPHERS
 
 # If curl flags are added, this determines whether they should be appended to our default flags (default),
@@ -219,7 +236,7 @@ readonly BADBLOCK_CURL_FLAGS_OVERRIDE
 export BADBLOCK_CURL_FLAGS_OVERRIDE
 
 # curl flags
-readonly BADBLOCK_CURL_FLAGS_DEFAULT="--disable --no-netrc --ciphers ${BADBLOCK_CIPHERS} --clobber --create-dirs --delegation none --disallow-username-in-url --doh-cert-status --fail --ftp-create-dirs --ftp-ssl-control --junk-session-cookies --no-basic --no-ca-native --no-digest --no-doh-insecure --no-http0.9 --no-insecure --no-proxy-insecure --no-negotiate --no-ntlm --no-proxy-basic --no-proxy-ca-native --no-proxy-digest --no-proxy-insecure --no-proxy-ssl-allow-beast --no-proxy-ssl-auto-client-cert --no-sessionid --no-skip-existing --no-ssl --no-ssl-allow-beast --no-ssl-auto-client-cert --no-ssl-no-revoke --no-ssl-revoke-best-effort --no-tls-earlydata --no-xattr --progress-meter --proto -all,https --proto-default https --proto-redir -all,https --referer "" --remove-on-error --show-error --ssl-reqd --tlsv1.2 --trace-time --user-agent "" --verbose"
+readonly BADBLOCK_CURL_FLAGS_DEFAULT="--disable --no-netrc --ciphers ${BADBLOCK_NONTLS13_CIPHERS} --clobber --create-dirs --delegation none --disallow-username-in-url --doh-cert-status --fail --fail-early --junk-session-cookies --no-basic --no-ca-native --no-digest --no-doh-insecure --no-http0.9 --no-insecure --no-negotiate --no-ntlm --no-proxy-basic --no-proxy-ca-native --no-proxy-digest --no-proxy-insecure --no-proxy-negotiate --no-proxy-ssl-auto-client-cert --no-sessionid --no-ssl-auto-client-cert --no-ssl-no-revoke --no-ssl-revoke-best-effort --no-xattr --parallel --post301 --post302 --post303 --progress-meter --proto -all,https --proto-default https --proto-redir -all,https --proxy-ciphers ${BADBLOCK_NONTLS13_CIPHERS} --proxy-tls13-ciphers ${BADBLOCK_TLS13_CIPHERS} --referer '' --remove-on-error --retry 5 --retry-all-errors --retry-connrefused --show-error --tls13-ciphers ${BADBLOCK_TLS13_CIPHERS} --tlsv1.2 --trace-time --user-agent '' --verbose"
 if [[ -z "${BADBLOCK_CURL_FLAGS+x}" ]]; then
     BADBLOCK_CURL_FLAGS="${BADBLOCK_CURL_FLAGS_DEFAULT}"
 elif [[ "${BADBLOCK_CURL_FLAGS_OVERRIDE}" == 1 ]]; then
