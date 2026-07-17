f8987651d518739e14b861ebbb590d13f7fad8b0
celenity <celenity@celenity.dev>
2026-07-17 05:57:54 +0000

    fix: remove unnecessary `local readonly extension`
    Signed-off-by: celenity <celenity@celenity.dev>

--- scripts/get_sources-bb.sh
+++ scripts/get_sources-bb.sh
@@ -419,7 +419,6 @@ function download_and_extract() {
     fi
   fi
 
-  local readonly extension
   if [[ "${url}" =~ \.tar\.xz$ ]]; then
     local readonly extension=".tar.xz"
   elif [[ "${url}" =~ \.tar\.gz$ ]]; then
